2981580d3dc21d5b938550a8c8574b15df4e41a8
maria <147421482+mariaR5@users.noreply.github.com>
2026-07-16 22:43:46 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int arr[26] = {0};
+        for (char ch: s) {
+            for (int i = 0; i < 26; i++)
+                arr[ch -'a']++;
+        }
+        for (char ch: t) {
+            for (int i = 0; i < 26; i++) arr[ch -'a']--;
+        }
+         for (int i: arr) {
+            if (i != 0) return false;
+        }
+        return true;
+    }
+};
